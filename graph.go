70fb2d59e6c32db2d6312b647d8253dec47000bc
Maxence PATOUILLERE <maxence.patouillere@ynov.com>
2022-10-15 18:44:46 +0200

    edit graph.go: update printHangMan func

--- graph.go
+++ graph.go
@@ -1,89 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func printHangMan(failAttempts int) {
-	switch {
-	case failAttempts == 1:
-		for i := 0; i < 7; i++ {
-			fmt.Print("\n")
-		}
-		fmt.Println("=========")
-	case failAttempts == 2:
-		for i := 0; i < 5; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 3:
-		fmt.Println("  +---+")
-		for i := 0; i < 5; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 4:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		for i := 0; i < 4; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 5:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		for i := 0; i < 3; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 6:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		fmt.Println("  |   |")
-		for i := 0; i < 2; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 7:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		fmt.Println(" /|   |")
-		for i := 0; i < 2; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 8:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		fmt.Println(" /|\\  |")
-		for i := 0; i < 2; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 9:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		fmt.Println(" /|\\  |")
-		fmt.Println(" /    |")
-		for i := 0; i < 1; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
-	case failAttempts == 10:
-		fmt.Println("  +---+")
-		fmt.Println("  |   |")
-		fmt.Println("  O   |")
-		fmt.Println(" /|\\  |")
-		fmt.Println(" / \\  |")
-		for i := 0; i < 1; i++ {
-			fmt.Println("      |")
-		}
-		fmt.Println("=========")
+	var f *os.File
+	f, _ = os.Open("hangman.txt")
+	scanner := bufio.NewScanner(f)
+	var result []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, line)
+	}
+	for i := 0; i < 8; i++ {
+		fmt.Println(result[i+8*(failAttempts-1)])
 	}
 	fmt.Println("Not present in the word,", 10-failAttempts, "attempts remaining")
 }
