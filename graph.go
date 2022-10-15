20372fdb19b10c5f2d40ffde358ce584591fa8cd
Maxence PATOUILLERE <maxence.patouillere@ynov.com>
2022-10-15 18:41:06 +0200

    edit graph.go: finish rework

--- graph.go
+++ graph.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func printHangMan(failAttempts int) {
-
+	var f *os.File
+	f, _ = os.Open("hangman.txt")
+	scanner := bufio.NewScanner(f)
+	var result []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, line)
+	}
+	for i := 0; i < 8; i++ {
+		fmt.Println(result[i+8*(failAttempts-1)])
+	}
 	fmt.Println("Not present in the word,", 10-failAttempts, "attempts remaining")
 }
